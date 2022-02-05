d29b4daa23ab2f1383aff0b4e545c8c643ab412d
Arthur van der Leest <avdleest@gmail.com>
2022-02-05 21:04:03 +0100

    Add postSlice
    Adds the postSlice and some initialState to play with.

--- src/features/communities/communitiesSlice.js
+++ src/features/communities/communitiesSlice.js
@@ -1,10 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+  1: {
+    id: 1,
+    name: 'Cool Community',
+    src: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+  },
+}
+
 const communitiesSlice = createSlice({
   name: 'communities',
-  initialState: {
-
-  },
+  initialState,
   reducers: {
     addCommunity: (state, action) => {
       state[action.payload.id] = action.payload
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import communitiesReducer from '../features/communities/communitiesSlice'
+import postsReducer from '../features/posts/postsSlice'
 
 const store = configureStore({
   reducer: {
     communities: communitiesReducer,
+    posts: postsReducer,
   },
 })
 
