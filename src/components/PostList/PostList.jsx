e25085273c7a2f61074fac6fa02394f26db05cb8
fql <77008470+fengql123@users.noreply.github.com>
2022-02-07 14:31:36 +0800

    Done with post styling

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -7,8 +7,17 @@ import './PostList.css'
 export default function PostList({ posts }) {
   return (
     <div className="postList">
-      {posts.map((data) =>
-        <Post title={data.title} text={data.selftext} pic={data.url_overridden_by_dest} />)}
+      {posts.map((data) => (
+        <Post
+          title={data.title}
+          text={data.selftext}
+          pic={data.url_overridden_by_dest}
+          user={data.author}
+          date={new Date(data.created)}
+          num_comments={data.num_comments}
+          comments={data.url}
+        />
+      ))}
     </div>
   )
 }
